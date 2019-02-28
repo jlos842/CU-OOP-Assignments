2810cb98cf9a4c6f25e0a20a78c5a654f4b93c4e
Joe Los <jlos484@gmail.com>
2019-02-28 12:03:44 -0700

    Create files for store class

---
+++ OOP3/Store.h
@@ -0,0 +1,21 @@
+/*
+Josef Los
+
+Header file for the store class. The store manages a list of Tools as well
+as a vector of Rental objects.
+*/
+
+#ifndef STORE_H
+#define STORE_H
+
+#include "Tool.h"
+
+class Store
+{
+	public:
+	private:
+		int moneyEarned;
+
+};
+
+#endif
